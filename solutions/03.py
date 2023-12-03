7e934250d53ae2c36f61aa16da6fcaf9d0112e15
Johann Gerberding <johann.gerberding@lector.ai>
2023-12-03 15:27:08 +0100

    day 3 part 2

--- solutions/03.py
+++ solutions/03.py
@@ -1,3 +1,4 @@
+from math import prod 
 
 def get_surrounding(x: int, y: int, max_x: int, max_y: int) -> list: 
     coords = [
@@ -71,6 +72,55 @@ def check_engine(example: list):
 
     print(result)                
 
+
+def check_gears(example: list):
+    numbers = [] 
+    symbols = [] 
+
+    max_x = len(example[0]) - 1 
+    max_y = len(example) - 1 
+    
+    for row, ex in enumerate(example):
+        # first all symbols 
+        for col, c in enumerate(ex):  
+            if not c.isdigit() and c != '.':
+                symbols.append((c, (row, col)))
+
+        # replace symbols 
+        ex_list = list(ex)
+        for el in symbols:
+            if el[1][0] == row:
+                ex_list[el[1][1]] = "."
+        ex = ''.join(ex_list)
+        # then get numbers  
+        nums = [el for el in ex.split(".") if el != '']
+        
+        idxs = []  
+        curr_idx = 0 
+        for num in nums:  
+            idx = ex[curr_idx:].index(num)
+            idxs.append(idx + curr_idx)
+            curr_idx += (idx + len(num)) 
+        for num, idx in zip(nums, idxs): 
+            numbers.append((int(num), [(row, idx + col) for col in range(len(num))]))
+    # go through the symbols 
+    # check in every number, if the symbol is a neighbour
+    # if the symbol has two neighbours, its a gear  
+    gears = []
+    for val, pos in symbols: 
+        gear = [] 
+        if val == '*': 
+            for num, num_pos in numbers: 
+                neighbours = get_num_surrounding(num_pos, max_x, max_y)
+                if pos in neighbours:
+                    # yay its a neighbour
+                    gear.append(num)
+        if len(gear) > 1:
+            gears.append(prod(gear))
+
+    print(sum(gears))
+
+
 example = """467..114..
 ...*......
 ..35..633.
@@ -90,3 +140,7 @@ with open("./inputs/03.txt", 'r') as fp:
     inp = [el.strip() for el in fp.readlines()]
 
 check_engine(inp)
+
+check_gears(example)
+
+check_gears(inp)
