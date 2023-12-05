d90c0052bc4c7ef875b547cb1352d9d5d24c099f
johanngerberding <33900162+johanngerberding@users.noreply.github.com>
2023-12-05 19:04:31 +0100

    day 5 part 1

--- python/solutions/05.py
+++ python/solutions/05.py
@@ -1,3 +1,31 @@
+import tqdm 
+
+
+def solve(example):
+    example = [el.split(":") for el in example.split("\n\n")]
+    example = [[el[0], el[1].strip().split("\n")] for el in example]
+    seeds = [int(el) for el in example[0][1][0].split(' ')]
+    maps = example[1:]
+    maps = [[el[0], [[int(num) for num in nums.split(' ')] for nums in el[1]]] for el in maps]
+
+    # map format 
+    # [map_name, [[destination range start, source range start, the range length], ... ]]
+
+    def transform(num: int, map_info: list) -> int:  
+        for info in map_info:
+            if info[1] <= num < info[1] + info[2]:
+                return info[0] + num - info[1] 
+        return num        
+
+    for m in maps:  
+        # s2d = source2dest(map_info=m[1])
+        nseed = [] 
+        for seed in seeds:
+            nseed.append(transform(seed, m[1]))
+        seeds = nseed
+
+    print(min(seeds))
+
 
 example = """seeds: 79 14 55 13
 
@@ -33,6 +61,9 @@ humidity-to-location map:
 60 56 37
 56 93 4"""
 
-example = [el.split(":") for el in example.split("\n\n")]
+with open("../inputs/05.txt", 'r') as fp: 
+    inp = fp.read()
 
-print(example)
+
+solve(example=example)
+solve(example=inp)
