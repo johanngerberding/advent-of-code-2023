edec5fca15b09ae4aa77df00ff730f53452d75d8
johanngerberding <33900162+johanngerberding@users.noreply.github.com>
2023-12-15 17:46:51 +0100

    day 9 part 2

--- python/solutions/09.py
+++ python/solutions/09.py
@@ -14,16 +14,36 @@ def run(inp: str):
     lines = [[int(el) for el in li.split(" ")] for li in lines]
     result = 0
     for line in lines:
-        lasts = [] 
-        lasts.append(line[-1])
+        lasts = 0 
+        lasts += line[-1]
         curr = line.copy() 
         while not check_zero(curr):
             curr = get_diffs(curr)
             if len(curr) > 0: 
-                lasts.append(curr[-1])
-        result += sum(lasts) 
+                lasts += curr[-1]
+        result += lasts 
 
     print(f"Result: {result}")
+
+def run2(inp: str):
+    lines = inp.split("\n")
+    lines = [[int(el) for el in li.split(" ")] for li in lines]
+    result = 0
+    for line in lines:
+        firsts = [] 
+        firsts.append(line[0])
+        curr = line.copy() 
+        while not check_zero(curr):
+            curr = get_diffs(curr)
+            if len(curr) > 0: 
+                firsts.append(curr[0])
+        firsts = list(reversed(firsts))
+        sub = 0  
+        for i in range(1, len(firsts)):
+            sub = firsts[i] - sub 
+        result += sub  
+    print(f"Result: {result}")
+
 
 
 example = """0 3 6 9 12 15
@@ -31,7 +51,10 @@ example = """0 3 6 9 12 15
 10 13 16 21 30 45"""
 run(example)
 
+run2(example)
+
 with open("../inputs/09.txt", 'r') as fp:
     data = fp.read()
 
 run(data)
+run2(data)
