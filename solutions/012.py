2fd262404186113e6d75665bbb0a7cb5e02f3e59
johanngerberding <johann.gerberding@lector.ai>
2023-12-21 16:25:21 +0100

    part 1 day 12

--- solutions/012.py
+++ solutions/012.py
@@ -1,16 +1,41 @@
 from itertools import combinations_with_replacement, permutations, product
 
 
-def valid(line: str, parts: list):
-    ...
+def valid(line: str, parts: list) -> bool:
+    elements = [el for el in line.split(".") if el != '']
+    if len(elements) != len(parts):
+        return False
+    for i, element in enumerate(elements): 
+        if not len(element) == parts[i]:
+            return False
+    return True
+
+def get_combinations(line: str) -> tuple:
+    idxs = [i for i, el in enumerate(line) if el == '?']
+    return list(product('.#', repeat=len(idxs))), idxs 
 
 
-def get_combinations(line: str) -> list:
-    num_elements = len([el for el in line if el == '?'])
-    print(num_elements) 
-    return list(product('.#', repeat=num_elements))
+def solve(inp: str):
+    example = inp.split("\n")
+    example = [row.split(" ") for row in example]
+    example = [[el[0], [int(num) for num in el[1].split(",")]] for el in example]
+    result = 0 
+    for line in example:
+        combinations, idxs = get_combinations(line[0])
+        valid_combinations = 0 
+        for combination in combinations: 
+            nline = list(line[0])
+            for idx, el in zip(idxs, combination): 
+                nline[idx] = el
+            if valid(''.join(nline), line[1]): 
+                valid_combinations += 1
 
+        result += valid_combinations
 
+    return result
+
+# brute force?? 
+# try all possible combinations and check if valid?? 
 example = """???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -18,13 +43,11 @@ example = """???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1"""
 
+example_res = solve(example)
+print(f"Example result: {example_res}")
 
-example = example.split("\n")
-example = [row.split(" ") for row in example]
-example = [[el[0], [int(num) for num in el[1].split(",")]] for el in example]
-print(example)
-combinations = get_combinations(example[0][0])
-print(combinations)
-# brute force?? 
-# try all possible combinations and check if valid?? 
+with open("../inputs/012.txt", 'r') as fp: 
+    data = fp.read()
 
+part1 = solve(data)
+print(f"Part 1: {part1}")
