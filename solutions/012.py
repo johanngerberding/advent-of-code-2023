14dd4717fc8fa94ee1f46c5c87bc8c9de29a525b
johanngerberding <johann.gerberding@lector.ai>
2023-12-22 11:59:45 +0100

    strugglnig with day 12 part 2

--- solutions/012.py
+++ solutions/012.py
@@ -1,6 +1,18 @@
-from itertools import combinations_with_replacement, permutations, product
+from itertools import product
+import time 
 
 
+def timer(func): 
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        val = func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        if elapsed > 1.: 
+            print(f"{func.__name__} took {elapsed:.2f}s")
+        return val 
+    return wrapper 
+
+@timer
 def valid(line: str, parts: list) -> bool:
     elements = [el for el in line.split(".") if el != '']
     if len(elements) != len(parts):
@@ -10,11 +22,14 @@ def valid(line: str, parts: list) -> bool:
             return False
     return True
 
-def get_combinations(line: str) -> tuple:
+# this is the problem, its too complex and inefficient 
+@timer
+def get_combinations(line: str, parts: list) -> tuple:
     idxs = [i for i, el in enumerate(line) if el == '?']
     return list(product('.#', repeat=len(idxs))), idxs 
 
 
+@timer
 def extend(inp: str) -> list:
     example = inp.split("\n")
     example = [row.split(" ") for row in example]
@@ -40,9 +55,11 @@ def solve(inp: str, part1=True):
     else: 
         example = extend(inp) 
     result = 0 
-    for line in example:
-        combinations, idxs = get_combinations(line[0])
+    for i, line in enumerate(example):
+        print(f"Lets get all the combinations of line {i}")
+        combinations, idxs = get_combinations(line[0], line[1])
         valid_combinations = 0 
+        print(f"Now let's check all the combinations")
         for combination in combinations: 
             nline = list(line[0])
             for idx, el in zip(idxs, combination): 
@@ -63,14 +80,18 @@ example = """???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1"""
 
-example_res = solve(example)
-print(f"Example result: {example_res}")
 
-with open("../inputs/012.txt", 'r') as fp: 
-    data = fp.read()
+# example_res = solve(example)
+# print(f"Example result: {example_res}")
 
-part1 = solve(data)
-print(f"Part 1: {part1}")
+# with open("../inputs/012.txt", 'r') as fp: 
+#     data = fp.read()
 
-part2 = solve(data, part1=False)
+# part1 = solve(data)
+# print(f"Part 1: {part1}")
+
+part2 = solve(example, part1=False)
 print(f"Part 2 example: {part2}")
+
+# brute force works for the first part but not for the second
+# use dynamic programming to solve the second part?? 
