5e7afb2ce0108ca1ae7e94a68f38621be2447f18
johanngerberding <33900162+johanngerberding@users.noreply.github.com>
2023-12-28 10:29:46 +0100

    day 14 part 2

--- solutions/014.py
+++ solutions/014.py
@@ -1,17 +1,25 @@
-
+import tqdm
 
 # for every line, stops are 0 + the row of #
 # for every col you go from 0 -> end and check whether the piece can move up or not
 
 # directions: 
 # 0 -> north
-# 1 -> east 
+# 3 -> east 
 # 2 -> south 
-# 3 -> west
+# 1 -> west
 
 class Grid:
     def __init__(self, inp: str):
         self.grid = [[c for c in el] for el in inp.split("\n")]
+        self.memory = {''.join([''.join(row) for row in self.grid]): 0} 
+    
+    def __repr__(self):
+        rows = [''.join(row) for row in self.grid] 
+        out = "" 
+        for row in rows:
+            out += (row + "\n")
+        return out 
 
     def transform(self, direction: int):
         transformed = self.grid.copy() 
@@ -31,20 +39,58 @@ class Grid:
             for row in range(len(self.grid)):
                 for col in range(len(self.grid[0])):
                     transformed[row][col] = cols[col][row] 
+            
+
         
         # east -> row operations
-        elif direction == 1:
-            ...
+        elif direction == 3:
+            rows = []
+            for r in self.grid:
+                row = ''.join(r)
+
+                subparts = row.split("#")
+                subparts = [''.join(sorted(el)) for el in subparts] 
+                new_row = '#'.join(subparts)
+                rows.append(new_row)
+            
+            for row in range(len(self.grid)):
+                for col in range(len(self.grid[0])):
+                    transformed[row][col] = rows[row][col] 
+
         # south -> col operations
         elif direction == 2:
-            ...
+            cols = [] 
+            for col in range(len(self.grid[0])): 
+                column = "" 
+                for row in range(len(self.grid)):
+                    column += self.grid[row][col]
+
+                subparts = column.split("#")
+                subparts = [''.join(sorted(el)) for el in subparts] 
+                new_column = '#'.join(subparts)
+                cols.append(new_column)
+            
+            for row in range(len(self.grid)):
+                for col in range(len(self.grid[0])):
+                    transformed[row][col] = cols[col][row] 
         # west -> row operations
-        elif direction == 3:
-            ...
+        elif direction == 1:
+            rows = []
+            for r in self.grid:
+                row = ''.join(r)
+
+                subparts = row.split("#")
+                subparts = [''.join(sorted(el, reverse=True)) for el in subparts] 
+                new_row = '#'.join(subparts)
+                rows.append(new_row)
+            
+            for row in range(len(self.grid)):
+                for col in range(len(self.grid[0])):
+                    transformed[row][col] = rows[row][col] 
         else: 
             raise ValueError("Please give a valid direction value.")
     
-        self.grid = transformed
+        return transformed
     
     def load(self) -> int: 
         result = 0 
@@ -53,19 +99,28 @@ class Grid:
             result += sum([1 for el in row if el == 'O']) * score 
         return result
 
-# def parse(inp: str):
-#     rows = inp.split("\n") 
-#     cols = []
-#     # get col values
-#     for c in range(len(rows[0])):    
-#         col = "" 
-#         for i in range(len(rows)):
-#             col += rows[i][c]
-#         cols.append(col)
-
-#     ...
-
-
+    def cycle(self, reps: int): 
+        for i in range(reps): 
+            for k in range(4):
+                transformed = self.transform(k)
+                grid = ''.join([''.join(row) for row in self.grid])
+                if grid not in self.memory:
+                    self.memory[grid] = i * 4 + k + 1
+                    self.grid = transformed 
+                else: 
+                    pos = self.memory[grid] 
+                    curr = i * 4 + k + 1 
+                    pattern = curr - pos 
+                    rest = reps * 4 - pos 
+                    mod = rest % pattern 
+                    for _ in range(mod):
+                        transformed = self.transform(k)
+                        self.grid = transformed 
+                        if k <= 2:
+                            k += 1
+                        else: 
+                            k = 0 
+                    return 
 
 example = """O....#....
 O.OO#....#
@@ -80,16 +135,16 @@ O.#..O.#.#
 
 grid = Grid(example)
 
-print(grid.grid)
-grid.transform(0)
-print(grid.grid)
+grid.cycle(1_000_000_000)
+print(grid)
+
 score = grid.load()
-print(f"example -> {score}")
+print(f"example part 2 -> {score}")
 
 with open("../inputs/014.txt", "r") as fp:
     data = fp.read()
 
 grid = Grid(data)
-grid.transform(0)
-part1 = grid.load()
-print(f"part1: {part1}")
+grid.cycle(1_000_000_000)
+score = grid.load()
+print(f"part 2: {score}")
