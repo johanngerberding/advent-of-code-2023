1329be57f1af7c4ecb2a80f793c2d43a00f27bc4
johanngerberding <33900162+johanngerberding@users.noreply.github.com>
2023-12-28 16:04:59 +0100

    day 16 part 2

--- solutions/016.py
+++ solutions/016.py
@@ -21,18 +21,20 @@ def next_pos(curr: tuple, direction: int, max_col: int, max_row: int) -> tuple |
         return (pos[0], pos[1], direction) 
     return None
 
-def solve(inp: str):
+
+def parse(inp: str): 
     grid = []
     example = inp.split("\n")
     for row in example: 
         grid.append([el for el in row])
+    return grid
 
+def solve(grid: list, start: tuple):
     max_row = len(grid) - 1 
     max_col = len(grid[0]) - 1 
     visited = set()
     # direction 0 -> right, 1 -> down, 2 -> left, 3 -> up
-    stack = [(0, 0, 0)]
-
+    stack = [start]
 
     while len(stack) > 0:
         curr = stack.pop()
@@ -80,11 +82,10 @@ def solve(inp: str):
                 if n is not None: 
                     stack.append(n)
 
-
     positions = set()
     for x, y, _ in visited: 
         positions.add((x,y))
-    print(len(positions))
+    return len(positions)
 
 
 example = r""".|...\....
@@ -98,9 +99,31 @@ example = r""".|...\....
 .|....-|.\
 ..//.|...."""
 
-solve(example)
+example_grid = parse(example)
+solve(example_grid, (0, 0, 0))
+
+def solve2(grid: list):
+    down = [(0, i, 1) for i in range(len(grid[0]))]
+    up = [(len(grid) - 1, i, 3) for i in range(len(grid[-1]))]
+    left = [(i, 0, 0) for i in range(len(grid))]
+    right = [(i, len(grid) - 1, 2) for i in range(len(grid))]
+    starts = down + up + left + right 
+
+    max_vis = 0
+    for start in starts: 
+        vis = solve(grid, start)
+        max_vis = vis if vis > max_vis else max_vis
+
+    return max_vis
+
+ex_part2 = solve2(example_grid)
+print(f"Example part 2: {ex_part2}")
 
 with open("../inputs/016.txt", "r") as fp: 
     data = fp.read()
 
-solve(data)
+data_grid = parse(data)
+part1 = solve(data_grid, (0, 0, 0))
+print(f"part 1: {part1}")
+part2 = solve2(data_grid)
+print(f"part 2: {part2}")
