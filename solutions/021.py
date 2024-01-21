1881ef3daf58e4a3ed41a37832171f1fd7f85a14
johanngerberding <33900162+johanngerberding@users.noreply.github.com>
2024-01-21 09:37:43 +0100

    works for 6 and 10 steps but not higher

--- solutions/021.py
+++ solutions/021.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 # build a tree based on the map and count all nodes
 # 64 steps, 1 -> 4 -> 16 -> 64 -> ...
@@ -13,11 +14,44 @@ data = """...........
 .......##..
 .##.#.####.
 .##..##.##.
-...........
-"""
+..........."""
 
-with open("../inputs/021.txt", "r") as fp: 
-    data = fp.read()
+data = """.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##..S####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+................................."""
+
+#with open("../inputs/021.txt", "r") as fp: 
+#    data = fp.read()
 
 example_map = [[c for c in line] for line in data.split("\n")]
 
@@ -25,49 +59,68 @@ start = None
 for row, line in enumerate(example_map):
     for col, c in enumerate(line): 
         if c == 'S':
-            start = (row, col, 'S')
+            start = (row, col, 'S', 0, 0)
 
 assert start is not None
 positions = [start]
-tree = {start: set()} 
-steps = 0
-
-def get_height(node: tuple):
-    if node not in tree:
-        return 0
-    if len(tree[node]) == 0:
-        return 0 
-    return max([get_height(adj) for adj in tree[node]]) + 1
 
 steps = 0 
 while True:
     steps += 1 
-    new_positions = [] 
+    new_positions = set() 
     while positions:
-        row, col, val = positions.pop()
+        # track the positions relative to 'S' ??? 
+        row, col, val, rel_row, rel_col = positions.pop()
+
+        if row - 1 >= 0:
+            up_row = row - 1
+            up_col = col 
+            up = example_map[up_row][up_col]
+        else: 
+            up_row = len(example_map) - 1
+            up_col = col
+            up = example_map[up_row][up_col] 
         
-        up = example_map[row - 1][col] if row-1 >= 0 else None
-        down = example_map[row + 1][col] if row+1 < len(example_map[0]) - 1 else None 
-        left = example_map[row][col - 1] if col-1 >= 0 else None
-        right = example_map[row][col + 1] if col+1 < len(example_map) - 1 else None
+        if row + 1 < len(example_map) - 1:
+            down_row = row + 1
+            down_col = col 
+            down = example_map[down_row][down_col] 
+        else: 
+            down_row = 0 
+            down_col = col
+            down = example_map[down_row][down_col] 
+
+        if col - 1 >= 0:
+            left_row = row 
+            left_col = col - 1
+            left = example_map[left_row][left_col]        
+        else: 
+            left_row = row 
+            left_col = len(example_map[0]) - 1
+            left = example_map[left_row][left_col] 
         
-        #if (row, col, val) not in tree:
-        #    tree[(row, col, val, height)] = set() 
+        if col + 1 < len(example_map[0]) - 1:
+            right_row = row 
+            right_col = col + 1   
+            right = example_map[right_row][right_col] 
+        else: 
+            right_row = row 
+            right_col = 0
+            right = example_map[right_row][right_col]
 
         if up == '.' or up == 'S': 
-            new_positions.append((row-1, col, up))
+            new_positions.add((up_row, up_col, up, rel_row - 1, rel_col))
         if down == '.' or down == 'S':
-            new_positions.append((row+1, col, down))
+            new_positions.add((down_row, down_col, down, rel_row + 1, rel_col))
         if left == '.' or left == 'S':
-            new_positions.append((row, col-1, left))
+            new_positions.add((left_row, left_col, left, rel_row, rel_col - 1))
         if right == '.' or right == 'S':
-            new_positions.append((row, col+1, right))
+            new_positions.add((right_row, right_col, right, rel_row, rel_col + 1))
 
-    positions = list(set(new_positions))
-    if steps == 64: 
+    positions = list(new_positions)
+    if steps == 50: 
         break 
 
-# print(positions)
 print(len(positions))
 # for pos in positions: 
 #     row, col, val = pos 
@@ -75,3 +128,11 @@ print(len(positions))
 
 # for row in example_map: 
 #     print(''.join(row))
+
+model = np.polyfit(
+    [6, 10, 50, 100, 500, 1000, 5000], 
+    [16, 50, 1594, 6536, 167004, 668697, 16733044], 
+    2,
+)
+predict = np.poly1d(model)
+print(predict(26501365))
